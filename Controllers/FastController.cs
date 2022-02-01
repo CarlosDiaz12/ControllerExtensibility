604ef4127ab691a753a20f47e60fd3ef62842384
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-01 12:40:07 -0400

    page 523

---
+++ Controllers/FastController.cs
@@ -0,0 +1,26 @@
+﻿using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace ControllerExtensibility.Controllers
+{
+    // Using the SessionState Attribute
+    // Disable session for the controller
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class FastController : Controller
+    {
+        // GET: Fast
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Fast ",
+                ActionName = "Index"
+            });
+        }
+    }
+}
